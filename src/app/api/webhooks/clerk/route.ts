0c9bceeb032dac1bb64ee0e765b210be43bf4865
codezilar <zoeehimen@gmail.com>
2025-08-20 02:17:59 -0700

    Initial commit

--- src/app/api/webhooks/clerk/route.ts
+++ src/app/api/webhooks/clerk/route.ts
@@ -3,3 +3,8 @@ import {WebhookEvent} from "@clerk/nextjs/server"
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 import { Webhook } from "svix"
+
+import { createUser } from "@/lib/actions/user.action";
+export async function POST(req: Request) {
+    
+}
